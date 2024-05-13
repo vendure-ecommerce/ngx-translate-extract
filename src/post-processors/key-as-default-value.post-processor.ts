e35cfcccf4a72b9c3ba6ce5110ad4dc451d626a4
Steven <steven.gunneweg@aanzee.nl>
2024-05-13 13:28:18 +0200

    feat!(keep): Add check if key is pre-existing

--- src/post-processors/key-as-default-value.post-processor.ts
+++ src/post-processors/key-as-default-value.post-processor.ts
@@ -5,6 +5,6 @@ export class KeyAsDefaultValuePostProcessor implements PostProcessorInterface {
 	public name: string = 'KeyAsDefaultValue';
 
 	public process(draft: TranslationCollection, extracted: TranslationCollection, existing: TranslationCollection): TranslationCollection {
-		return draft.map((key: string, val: TranslationInterface): TranslationInterface => val.value === '' ? {value: key, sourceFiles: (val?.sourceFiles || [])} : val);
+		return draft.map((key: string, val: TranslationInterface): TranslationInterface => val.value === '' && !existing.has(key) ? {value: key, sourceFiles: (val?.sourceFiles || [])} : val);
 	}
 }
